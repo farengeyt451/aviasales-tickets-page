167b6fd14ec08b558c8f70a4dc0c53d094031f91
Alexander Kislov <strannik11.92@gmail.com>
2019-02-03 21:08:14 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header';
+import { FooterComponent } from './components/footer';
+import { TicketComponent } from './components/ticket';
+import { IndexPageComponent } from './components/index-page/index-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, TicketComponent, IndexPageComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
