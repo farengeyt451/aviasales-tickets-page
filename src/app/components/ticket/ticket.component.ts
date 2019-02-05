ccaccabbc181c29ca9141924f83cb7c039603b73
Alexander Kislov <strannik11.92@gmail.com>
2019-02-05 23:03:54 +0300

    Add new setDate method

--- src/app/components/ticket/ticket.component.ts
+++ src/app/components/ticket/ticket.component.ts
@@ -15,7 +15,13 @@ export class TicketComponent implements OnInit {
   }
 
   setDate(date: string): Date {
-    // let dateParts = date.split('.');
-    return new Date(date);
+    let dateParts = date.split('.');
+    let year = Number(`20${dateParts[2]}`);
+    console.log('year', year);
+    let month = Number(dateParts[1]);
+    console.log('month', month);
+    let day = Number(dateParts[0]);
+    console.log('day', day);
+    return new Date(year, month, day);
   }
 }
