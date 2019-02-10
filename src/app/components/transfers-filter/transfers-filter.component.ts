ecc313ea9c9bace43a528e9728d4d952569ec1c5
Alexander Kislov <strannik11.92@gmail.com>
2019-02-10 16:12:29 +0300

    Add func for data formating

--- src/app/components/transfers-filter/transfers-filter.component.ts
+++ src/app/components/transfers-filter/transfers-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { STOPS } from '../../fixtures/stops.fixture';
 import { IStops } from '../../interfaces/stops.interface';
@@ -6,7 +6,8 @@ import { IStops } from '../../interfaces/stops.interface';
 @Component({
   selector: 'app-transfers-filter',
   templateUrl: './transfers-filter.component.html',
-  styleUrls: ['./transfers-filter.component.sass']
+  styleUrls: ['./transfers-filter.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TransfersFilterComponent implements OnInit {
   stopsForm: FormGroup;
@@ -21,12 +22,12 @@ export class TransfersFilterComponent implements OnInit {
 
   ngOnInit() {}
 
-  // Геттер для рендера списка опций в шаблоне
+  // Getter for rendering a list of options in a template
   get stopsRender() {
     return this.stopsForm.get('stopsCount');
   }
 
-  // Возвращаем массив из FormControl
+  // Returning an array which consists from Form Controls
   buildStops() {
     const arr = this.stops.map(stop => {
       return this.fb.control(stop.selected);
@@ -34,15 +35,21 @@ export class TransfersFilterComponent implements OnInit {
     return this.fb.array(arr);
   }
 
-  submit(value) {
-    const formValue = Object.assign({}, value, {
-      stopsCount: value.stopsCount.map((el, i) => {
+  formatData(data) {
+    return data
+      .filter(el => el === true)
+      .map((el, i) => {
         return {
           id: this.stops[i].id,
           stopCount: this.stops[i].stopCount,
           selected: el
         };
-      })
+      });
+  }
+
+  submit(value) {
+    const formValue = Object.assign({}, value, {
+      stopsCount: this.formatData(value.stopsCount)
     });
     console.log(formValue);
   }
